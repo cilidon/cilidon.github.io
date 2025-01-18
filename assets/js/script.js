1e71bef690fea2836c3e7159ed253ba81ad04354
Mandar Mhatre <63152566+cilidon@users.noreply.github.com>
2025-01-18 22:13:31 +0000

    Changed version #

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,7 +49,7 @@ for (let i = 0; i < testimonialsItem.length; i++) {
 
 }
 
-// // add click event to modal close button
+// add click event to modal close button
 // modalCloseBtn.addEventListener("click", testimonialsModalFunc);
 // overlay.addEventListener("click", testimonialsModalFunc);
 
